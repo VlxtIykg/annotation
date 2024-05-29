6f5f7bc4326eb4d80b5c9485f0b04e49cb37395a
Kami <harshimymommy@gmail.com>
2024-05-28 18:44:19 -0700

    build(backend): 👷 Downloaded tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-	output: "server",
+  integrations: [react(), tailwind()],
+  output: "server"
 });
