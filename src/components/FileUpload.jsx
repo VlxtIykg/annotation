5fcb9c6449dff4b67a5dfb955865cbf6e45acbf2
Kami <harshimymommy@gmail.com>
2024-06-04 02:58:32 -0700

    style(frontend): 💄 Added tailwind classes to make things look nice

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -50,23 +50,25 @@ const FileUpload = () => {
 
 	return (
 		<div className=''>
-			<form onSubmit={handleSubmit}>
-				<fieldset>
-					<legend className='sr-only'>Choose file format</legend>
-					<div className='flex flex-wrap justify-center items-center'>
+				
+				<form onSubmit={handleSubmit}>
+					<fieldset>
+						<legend className='sr-only'>Choose file format</legend>
+						<div className='flex flex-wrap justify-center items-center'>
+							
+								<div className='flex flex-wrap justify-center'>
+									<div className='relative h-28 w-28 m-2'>
+										<label className='sr-only ' htmlFor="formatter">Speechmatics</label>
+										<div className='flex flex-col items-center justify-center h-full'>
+											<input className='absolute h-full w-full m-0 cursor-pointer z-10 opacity-0 peer/spm' type="radio" name="formatter" id="sms" onChange={handleRadioChange} />
+											<img src={speechmatic.src} alt="Speechmatics" className='w-16 h-16 peer-checked/spm:w-20 peer-checked/spm:h-20 peer-checked/spm:border-indigo-500 peer-checked/spm:border-solid peer-checked/spm:border-2' />
+											<p className='text-indigo-600 font-semibold text-sm tracking-wider'>speechmatics</p>
+										</div>
+									</div>	
+								</div>
+								
 							<div className='flex flex-wrap justify-center'>
 								<div className='relative h-28 w-28 m-2'>
-									<label className='sr-only ' htmlFor="formatter">Speechmatics</label>
-									<div className='flex flex-col items-center justify-center h-full'>
-								<input className='absolute h-full w-full m-0 cursor-pointer z-10 opacity-0 peer/spm' type="radio" name="formatter" id="sms" onChange={handleRadioChange} />
-								<img src={speechmatic.src} alt="Speechmatics" className='w-16 h-16 peer-checked/spm:w-20 peer-checked/spm:h-20 peer-checked/spm:border-indigo-500 peer-checked/spm:border-solid peer-checked/spm:border-2' />
-										<p className='text-indigo-600 font-semibold text-sm tracking-wider'>speechmatics</p>
-									</div>
-								</div>
-							</div>
-							
-					<div className='flex flex-wrap justify-center'>
-						<div className='relative h-28 w-28 m-2'>
 									<label className='sr-only' htmlFor="formatter">Whisper-x</label>
 									<div className='test flex flex-col items-center justify-center h-full -z-10'>
 										<input className='absolute h-full w-full m-0 cursor-pointer z-10 opacity-0 peer/wsx' type="radio" name="formatter" id="wsx"  onChange={handleRadioChange} />
@@ -75,15 +77,16 @@ const FileUpload = () => {
 									</div>
 								</div>
 							</div>
+
 						</div>
-					
-					<br />
-					
-					<legend className='sr-only'>Upload your file</legend>
-					<input  className="border" type="file" onChange={handleFileChange} />
-					<button className="border" type="submit">Upload</button>
-				</fieldset>
-			</form>
+						
+						<br />
+						
+						<legend className='sr-only'>Upload your file</legend>
+						<input  className="border" type="file" onChange={handleFileChange} />
+						<button className="border" type="submit">Upload</button>
+					</fieldset>
+				</form>
 			{result && <div id="diarized_text" className="max-w-prose personal_ct flex flex-wrap" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(result) }} />}
 		</div>
 	);
