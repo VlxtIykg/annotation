5f70b38ea4719c552a803b95f22bcfa1d507d158
Kami <harshimymommy@gmail.com>
2024-06-07 02:26:31 -0700

    feat(frontend+backend): ✨ Attempting to add a js cache feature! Not fully done
    Adding a js cache feature, not done at all, more to be updated
    
    Currently breaks when given a file that does not have values for speaker.

--- src/scripts/htmlformatter.js
+++ src/scripts/htmlformatter.js
@@ -1,4 +1,7 @@
+import cache from "js-cache";
+
 export default function htmlFormatter(data) {
+	console.log(data)
 	return data.map(obj => {
 		let htmlElements = Object.entries(obj).map(([key, value]) => generateHTML(key, value)).join('');
 		
@@ -8,11 +11,27 @@ export default function htmlFormatter(data) {
 }
 
 function generateHTML(key, value) {
+	console.log("Key: ", key, "Value: ", value)
+
+	if (key === "locale") return
 	if (key === 'text') {
 		if (value === '.') return '<p class="text">.<br/></p>';
 		if (value === ',') return '<p class="text">,<br/></p>';
 		if (value === ' ') return;
+		cache.get("Hewo", (v) => {
+			console.log({v})
+			return v
+		})		
 		return `<span class="space"> </span><p class="text">${value}</p>`;
-}
+	}
+	if (key === 'speaker') {
+		console.log(value.split("_")[1] ?? cache.get(key));
+		let specific_speaker_cls = "S" + parseInt(value.split("_")[1]);
+		return `<p class="speaker ${specific_speaker_cls}">Speaker ${value}</p>`;
+	}
+	cache.set(key, value);
   return `<p class="details ${key} ${value}">${key}: ${value}</p>`;
 }
+
+
+generateHTML()
