baf435a7806bc87ee70034469a89f699a486afca
Kami <harshimymommy@gmail.com>
2024-06-13 18:54:28 -0700

    fix(frontend+backend): 🚑️ Card showed no element or did not break line properly or only showed text in the card.
    The issue may reappear in other commits as the card is being redesigned from div to a modal

--- src/scripts/brkline.js
+++ src/scripts/brkline.js
@@ -2,19 +2,16 @@ export default function findUniqueSpeakers(e) {
 	const speakers = [];
 	const cards = document.getElementsByClassName('personal_cards');
 	
-	// ['S1', 'S1', 'S1', 'S1', 'S1', 'S2', 'S2', 'S2', 'S3', 'S1', 'S3', 'S2']
 	for (let card of cards) {
 		const speaker_number = card?.getElementsByClassName('speaker'); // S3
 		if (speaker_number === null || speaker_number === undefined) return;
-		const current_speaker = speaker_number[0].classList[2];
+		const current_speaker = speaker_number[0].classList[1];
 		const previous_speaker = speakers[speakers.length - 1];
 		if (previous_speaker === undefined) {
 			speakers.push(current_speaker);
 			continue;
 		}
 		if (previous_speaker !== current_speaker) {
-			
-			
 			const filler = document.createElement('div');
 			filler.classList.add('filler');
 			filler.classList.add('grow');
