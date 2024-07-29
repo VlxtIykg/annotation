3ddef4519b980b13970f33cb8e9744a2477906fb
irfan <irfanazbinmz@gmail.com>
2024-07-29 17:41:54 +0800

    changed most of the interface and functions adjusted

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,90 +1,96 @@
 import React, { useState, useEffect } from 'react';
 import DOMPurify from 'dompurify';
-import speechmatic from '@assets/speechmatic.svg';
-import wsx from '@assets/wsx.svg';
+import spm from '@assets/spms.svg';
+import whis from '@assets/whisp.svg';
 import findUniqueSpeakers from "@scripts/brkline.js";
 import cardActivator from '@scripts/card_activator.js';
 
 const FileUpload = () => {
-	const [file, setFile] = useState(null);
-	const [result, setResult] = useState('');
-	const [selectedFormatter, setSelectedFormatter] = useState(null);
+    const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState('');
+    const [result, setResult] = useState('');
+    const [selectedFormatter, setSelectedFormatter] = useState(null);
 
-	const handleRadioChange = (event) => {
-		setSelectedFormatter(event.target.id);
-	};
+    const handleRadioChange = (event) => {
+        setSelectedFormatter(event.target.id);
+    };
 
-	const handleFileChange = (event) => {
-		setFile(event.target.files[0]);
-	};
+    const handleFileChange = (event) => {
+        const selectedFile = event.target.files[0];
+        setFile(selectedFile);
+        if (selectedFile) {
+            const name = selectedFile.name;
+            const type = selectedFile.type.split('.').pop();
+            setFileName(`${name}`);
+        }
+    };
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		if (file) {
-			const formData = new FormData();
-			formData.append('file', file);
-			formData.append('formatter', selectedFormatter); 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+            formData.append('formatter', selectedFormatter); 
 
-			const response = await fetch('/api/upload', {
-				method: 'POST',
-				body: formData,
-			});
+            const response = await fetch('/api/upload', {
+                method: 'POST',
+                body: formData,
+            });
 
-			const data = await response.json();
-			setResult(data.message);
-		}
-	};
+            const data = await response.json();
+            setResult(data.message);
+        }
+    };
 
-	useEffect(() => {
-		if (result) {
-			cardActivator();
-			findUniqueSpeakers();
+    useEffect(() => {
+        if (result) {
+            cardActivator();
+            findUniqueSpeakers();
+        }
+    }, [result]);
 
-		}
-	}, [result]);
-
-	return (
-		<div className=''>
-				
-				<form onSubmit={handleSubmit}>
-					<fieldset>
-						<legend className='sr-only'>Choose file format</legend>
-						<div className='flex flex-wrap justify-center items-center'>
-							
-								<div className='flex flex-wrap justify-center'>
-									<div className='relative h-28 w-28 m-2'>
-										<label className='sr-only ' htmlFor="formatter">Speechmatics</label>
-										<div className='flex flex-col items-center justify-center h-full'>
-											<input className='absolute h-full w-full m-0 cursor-pointer z-10 opacity-0 peer/spm' type="radio" name="formatter" id="sms" onChange={handleRadioChange} />
-											<img src={speechmatic.src} alt="Speechmatics" className='w-16 h-16 peer-checked/spm:w-20 peer-checked/spm:h-20 peer-checked/spm:border-indigo-500 peer-checked/spm:border-solid peer-checked/spm:border-2' />
-											<p className='text-indigo-600 font-semibold text-sm tracking-wider'>speechmatics</p>
-										</div>
-									</div>	
-								</div>
-								
-							<div className='flex flex-wrap justify-center'> 
-								<div className='relative h-28 w-28 m-2'>
-									<label className='sr-only' htmlFor="formatter">Whisper-x</label>
-									<div className='test flex flex-col items-center justify-center h-full -z-10'>
-										<input className='absolute h-full w-full m-0 cursor-pointer z-10 opacity-0 peer/wsx' type="radio" name="formatter" id="wsx"  onChange={handleRadioChange} />
-										<img src={wsx.src} alt="Speechmatics" className='w-16 h-16 peer-checked/wsx:w-20 peer-checked/wsx:h-20 peer-checked/wsx:border-indigo-500 peer-checked/wsx:border-solid peer-checked/wsx:border-2' />
-										<p className='text-indigo-600 font-semibold text-sm tracking-wider'>whisper-x</p>
-									</div>
-								</div>
-							</div>
-
-						</div>
-						
-						<br />
-						
-						<legend className='sr-only'>Upload your file</legend>
-						<input  className="border" type="file" onChange={handleFileChange} />
-						<button className="border" type="submit">Upload</button>
-					</fieldset>
-				</form>
-			{result && <div id="diarized_text" className="max-w-prose personal_ct flex flex-wrap" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(result) }} />}
-		</div>
-	);
+    return (
+        <div className=''>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <legend className='sr-only'>Choose file format</legend>
+                    <div className='flex flex-wrap justify-center'>
+                        <div className='flex flex-wrap justify-center'>
+                            <div className='relative h-28 w-28 m-2'>
+                                <label className='sr-only ' htmlFor="formatter">Speechmatics</label>
+                                <div className='flex flex-col items-center justify-center h-full'>
+                                    <input className='absolute h-full w-full m-0 cursor-pointer z-10 opacity-0 peer/spm' type="radio" name="formatter" id="sms" onChange={handleRadioChange} />
+                                    <img src={spm.src} alt="Speechmatics" className='w-20 peer-checked/spm:w-20 peer-checked/spm:h-20 peer-checked/spm:border-indigo-500 peer-checked/spm:border-solid peer-checked/spm:border-2' />
+                                    <p className='text-white-600 text-sm tracking-wide'>Speechmatics</p>
+                                </div>
+                            </div>    
+                        </div>
+                        <div className='flex flex-wrap justify-center'> 
+                            <div className='relative h-28 w-28 m-2'>
+                                <label className='sr-only' htmlFor="formatter">Whisper-x</label>
+                                <div className='test flex flex-col items-center justify-center h-full -z-10'>
+                                    <input className='absolute h-full w-full cursor-pointer z-10 opacity-0 peer/wsx' type="radio" name="formatter" id="wsx"  onChange={handleRadioChange} />
+                                    <img src={whis.src} alt="Whisper-x" className='w-20 peer-checked/wsx:w-20 peer-checked/wsx:h-20 peer-checked/wsx:border-indigo-500 peer-checked/wsx:border-solid peer-checked/wsx:border-2' />
+                                    <p className='text-white-600 text-sm tracking-wide'>Whisper-x</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br/>
+                    <legend className='sr-only'>Upload your file</legend>
+                    <div className="border-2 bg-black rounded-lg w-fit mx-auto my-0 fontify cursor-pointer">
+                        <input type="file" id="files"  className='hidden' onChange={handleFileChange}/>
+                        <label className="px-1 cursor-pointer" htmlFor="files">{fileName || 'Select file'}</label><br></br>
+                    </div>
+                    <div className='h-1 p-1'></div>
+                    <div className="border-2 bg-black px-1 rounded-lg w-fit mx-auto my-0 fontify">
+                        <button className='text-white-500' type="submit">Submit</button>
+                    </div>
+                </fieldset>
+            </form>
+            {result && <div id="diarized_text" className="max-w-prose personal_ct flex flex-wrap" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(result) }} />}
+        </div>
+    );
 };
 
 export default FileUpload;
