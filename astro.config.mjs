326238ed8613386dbbda89636aab5834ba131e4b
VlxtIykg <harshimymommy@gmail.com>
2024-07-30 01:09:47 -0700

    fix(backend): 🐛 Fix local dev unable to upload file, hoping it works for deployment soon!

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,17 +9,20 @@ import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
+// Docs: https://astro.build/config
+/**
+ * @type {import('astro/config').Config}
+ */
 export default defineConfig({
   site: "http://localhost:3000",
   integrations: [react(), tailwind()],
-  output: "hybrid",
-  adapter: node({mode: 'standalone',}),
-  vite: {
-    ssr: {
-      external: ["node:fs", "node:path", "node:buffer"],
-    }
-  },
-  prefetch: true,
+  output: "server",
+  adapter: node({mode: 'standalone'}),
+  // vite: {
+  //   ssr: {
+  //     external: ["node:fs", "node:path", "node:buffer"],
+  //   }
+  // },
+  // prefetch: true,
   server: { port: 3000 }
 });
