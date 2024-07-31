6ac118b153bdb6c2b3ae83fee828449c336acbca
VlxtIykg <harshimymommy@gmail.com>
2024-07-30 23:36:05 -0700

    feat(backend): ✨ Added a context handler to handle the file formats
    Still testing out why file is sent as [object Object] instead of [object Blob]
    
    Breaking changes: Cannot upload file yet

--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -10,45 +10,7 @@ export async function POST(context) {
     const file = formData.get("file");
     console.log(file);
     const type = formData.get("file").type;
-    switch (type) {
-      // Localhost receives JSON files as application/json;charset=utf-8 and as [object Blob]
-      case "application/json;charset=utf-8": {
-        const formatter = formData.get("formatter");
-        const json_file = await file.json();
-        const extrapolated_data = autoFill(json_file, formatter);
-        const html_str = htmlFormatter(extrapolated_data);
-        return new Response(JSON.stringify({ status: 200, message: html_str }));
-      }
-      /**
-       * Cloudflare receives
-       * JSON files as application/json and as [object Object] <- postman and site
-       * ZIP files as application/zip and as [object Blob]
-       */
-      case "application/json": {
-        const formatter = formData.get("formatter");
-        const json_file = await file.json();
-        const extrapolated_data = autoFill(json_file, formatter);
-        const html_str = htmlFormatter(extrapolated_data);
-        return new Response(JSON.stringify({ status: 200, message: html_str }));
-      }
-      case "application/zip": {
-        console.log(formData.get("zip"));
-        // Code for handling ZIP file
-        return;
-      }
-      case null:
-        console.log("No content type specified");
-        // Code for handling no content type not yet specified or file not uploaded
-        return new Response(
-          JSON.stringify({ status: 400, message: `No content type specified or no file sent` }),
-        );
-      default:
-        console.log("Unsupported content type");
-        // Code for handling files that cannot be parsed be it frontend or backend
-        return new Response(
-          JSON.stringify({ status: 400, message: `${json_file}<br>${type}<br><p>Unsupported content type</p><br>`}),
-        );
-    }
+    return context_Handler(file, type, formData);
   } catch (error) {
     console.error(error);
     return new Response(
@@ -65,4 +27,54 @@ export async function GET() {
   return new Response(
     JSON.stringify({ status: 200, message: "GET request received" }),
   );
+}
+
+/**
+ * 
+ * @param {Blob | JSON | JSON.stringify(file) } file Zip not supported yet, file sent to the parent function is sent here
+ * @param {string} type In forms of application/*, * being type i.e. zip, json, etc
+ * @param {boolean} skip Whether to skip entire switch case
+ * @returns {Response} Returns a response object to send back to original user
+ */
+export async function context_Handler(file, type, skip) {
+  if (skip) type = skip;
+  switch (type) {
+    // Localhost receives JSON files as application/json;charset=utf-8 and as [object Blob]
+    case "application/json;charset=utf-8": {
+      const formatter = formData.get("formatter");
+      const json_file = await file.json();
+      const extrapolated_data = autoFill(json_file, formatter);
+      const html_str = htmlFormatter(extrapolated_data);
+      return new Response(JSON.stringify({ status: 200, message: html_str }));
+    }
+    /**
+     * Cloudflare receives
+     * JSON files as application/json and as [object Object] <- postman and site
+     * ZIP files as application/zip and as [object Blob]
+     */
+    case "application/json": {
+      const formatter = formData.get("formatter");
+      const json_file = await file.json();
+      const extrapolated_data = autoFill(json_file, formatter);
+      const html_str = htmlFormatter(extrapolated_data);
+      return new Response(JSON.stringify({ status: 200, message: html_str }));
+    }
+    case "application/zip": {
+      console.log(formData.get("zip"));
+      // Code for handling ZIP file
+      return;
+    }
+    case null:
+      console.log("No content type specified");
+      // Code for handling no content type not yet specified or file not uploaded
+      return new Response(
+        JSON.stringify({ status: 400, message: `No content type specified or no file sent` }),
+      );
+    default:
+      console.log("Unsupported content type");
+      // Code for handling files that cannot be parsed be it frontend or backend
+      return new Response(
+        JSON.stringify({ status: 400, message: `${json_file}<br>${type}<br><p>Unsupported content type</p><br>`}),
+      );
+  }
 }
