d7f138e5ee034bb5e544e7ba33aff510601afc9a
Kami <harshimymommy@gmail.com>
2024-08-08 01:12:09 +0000

    fix(backend): 🚑️ Can't build CI accidentally had two get request!

--- src/pages/api/upload.js
+++ src/pages/api/upload.js
@@ -163,12 +163,3 @@ async function handleTryCatch(request, isFn = false) {
     return [null, err];
   }
 }
-
-export async function GET({ params, request }) {
-  return new Response(
-    JSON.stringify({
-      name: "Astro",
-      url: "https://astro.build/",
-    }),
-  );
-}
