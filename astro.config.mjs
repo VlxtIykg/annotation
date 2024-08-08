b175b4b8fbcbfd47b6d9df721cb4fd5afd30a87e
Kami <harshimymommy@gmail.com>
2024-08-08 01:14:44 +0000

    build(backend): 💚 Did not remember to change the adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,17 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // SSR Adapters
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // Docs: https://astro.build/config
 /**
  * @type {import('astro/config').Config}
  */
 export default defineConfig({
-  site: "http://localhost:3000",
+  site: "https://project1.kami.wtf",
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: node({ mode: "standalone" }),
+  adapter: cloudflare(),
   // prefetch: true,
   server: { port: 3000 },
 });
